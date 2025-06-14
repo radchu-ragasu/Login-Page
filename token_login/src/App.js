6d46142a23c057b13d641dd6e00ea69a801bbf25
radchu-ragasu <radsiga.20222082@iit.ac.lk>
2025-06-15 00:36:10 +0530

    File Structure

---
+++ token_login/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='Header'>
+      <label>Enter your email</label>
+      <input type="text" name="email" placeholder="Radchu@gmail.com"></input>
+    </div>
+  );
+}
+
+export default App;
