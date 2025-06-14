e945620ce803bf6215d19f8af109b2022f58e0d0
radchu-ragasu <radsiga.20222082@iit.ac.lk>
2025-06-15 00:43:54 +0530

    1 st implementation

--- token_login/src/App.js
+++ token_login/src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className='Header'>
-      <label>Enter your email</label>
-      <input type="text" name="email" placeholder="Radchu@gmail.com"></input>
-    </div>
+   <div className='center'>
+      <h1>Helllo Javascript </h1>
+      <p>I am on the way to catch you....</p>
+      <h2>This is a Login Page Using JWT Tokens</h2>
+      <h3>I am learning how its work and how can i use it efficiently</h3>
+      <legend>
+        <table className='space'>
+          <tr>
+            <td><label>User Name</label></td>
+            <td><input type="text" name="email" value="Radchu@gmail.com"></input></td>
+          </tr>
+        </table>
+        
+      </legend>
+   </div>
   );
 }
 
